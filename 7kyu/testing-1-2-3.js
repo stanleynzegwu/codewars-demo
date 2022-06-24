7fab281c65c0721fe881ae5115ad725d29d5a70b
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-06-24 06:58:02 +0100

    a function which takes a list of strings and returns each line prepended by the correct number

---
+++ 7kyu/testing-1-2-3.js
@@ -0,0 +1,25 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+//              SOLUTION
+
+function number(array){
+    let arr = []
+  array.forEach((a,b) => {
+      arr.push(`${b + 1}: ${a}`)
+  })
+    return arr
+}
+
+//          SOLUTION
+
+const number = array => array.map((n, i) => `${i + 1}: ${n}`)
