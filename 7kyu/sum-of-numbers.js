febbbb90e1485a8d643fe26506aa9c29cb9c398b
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-06-27 07:48:33 +0100

    a function that takes two intergers,either positive or negativethen sum the integers between and including them and return it

---
+++ 7kyu/sum-of-numbers.js
@@ -0,0 +1,29 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+//              solution
+
+function GetSum( a,b ) {
+    var result = 0;
+    var bigger = a > b ? a : b;
+    var smaller = a > b ? b : a;
+    for (var i = smaller; i <= bigger; i++) { result += i }
+    return result
+ }
+
+ //         solution
+
+ const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+  }
