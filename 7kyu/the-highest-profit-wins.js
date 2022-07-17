e02fc709b13e5efba2ca9b161cb2a1d7e520258a
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-07-17 10:53:11 +0100

    the highest profit wins

---
+++ 7kyu/the-highest-profit-wins.js
@@ -0,0 +1,20 @@
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// Examples (Input --> Output)
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+
+//              solution
+
+function minMax(arr){
+    array = []
+    array.push(Math.min(...arr),Math.max(...arr))
+    return array
+  }
+
+//              solution
+
+function minMax(arr){
+    return [Math.min(...arr), Math.max(...arr)];
+  }
