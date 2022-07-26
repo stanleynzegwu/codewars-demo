f12c1e203ff12541abf652b881423e53641ac81a
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-07-26 09:03:20 +0100

    indexed capitalization

---
+++ 7kyu/indexed-capitalization.js
@@ -0,0 +1,29 @@
+// Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+// For example:
+
+// capitalize("abcdef",[1,2,5]) = "aBCdeF"
+// capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+// The input will be a lowercase string with no spaces and an array of digits.
+
+//                  solution
+
+function capitalize(s,arr){
+    return s.split('').map((a,b) => arr.includes(b) ? a.toUpperCase() : a ).join('')
+  };
+
+//                  solution
+
+function capitalize(s,arr){
+  
+    var capS = s.split("");
+    
+    for(var i = 0; i < arr.length; i++) {
+      if(capS[arr[i]]) {
+        capS[arr[i]] = capS[arr[i]].toUpperCase();
+      }
+    }
+    
+    capS = capS.join("");
+    return capS
+    };
