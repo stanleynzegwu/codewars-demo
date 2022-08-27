469342bb0b274e900ba36072afcde8f0c4c3a260
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-08-27 07:55:54 +0100

    product of maximum of array

---
+++ 7kyu/product=of=maximum-of-array.js
@@ -0,0 +1,38 @@
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+// maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is  8 * 9 * 10 = 720 .
+// maxProduct ({10, 8, 3, 2, 1, 4, 10}, 5) ==> return (9600)
+// Explanation:
+// Since the size (k) equal 5 , then the subsequence of size 5 whose gives product of maxima is  10 * 10 * 8 * 4 * 3 = 9600 .
+// maxProduct ({-4, -27, -15, -6, -1}, 2) ==> return (4)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is  -4 * -1 = 4 .
+// maxProduct ({10, 3, -1, -27} , 3)  return (-30)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is 10 * 3 * -1 = -30 .
+
+//                  solution
+
+function maxProduct(numbers, size){
+    //sort the arr of numbers in descending order
+    arr = numbers.sort((a,b) => a-b)
+    
+    //using slice array method get size of the elements(the highest numbers
+    //will be at the end of the arr)
+    
+    //the use reduce ,ethod to get the product of the sliced value
+    return arr.slice(-size).reduce((acc,c) => acc*c,1)
+  }
