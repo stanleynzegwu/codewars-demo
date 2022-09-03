222d793a7d5986d94ac035514f39e1d086b3cce9
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-09-03 18:01:16 +0100

    recursion factorial

---
+++ 7kyu/recursion-factorial.js
@@ -0,0 +1,21 @@
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+// 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// The value of 0! is 1.
+
+// #Your task
+
+// You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+//                  solution
+
+const factorial = n => {
+    if(n <= 1 ) return 1
+    
+    return n * factorial(--n)
+  };
+
+//                  solution
+
+const factoria = n => n > 1 ? n*factorial(n-1) : 1;
