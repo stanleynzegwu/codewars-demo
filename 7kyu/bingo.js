0fde2bd2304948479350fab89d4b670c1bd80d10
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-10-03 07:26:22 +0100

    bingo

---
+++ 7kyu/bingo.js
@@ -0,0 +1,20 @@
+// For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+// Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO".
+//  They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+
+
+//                  solution
+
+const bingo = ar => [2,7,9,14,15].every(e => ar.includes(e)) ? 'WIN' : 'LOSE';
+
+//                  solution
+
+function bingo(a) {
+    let sum = 0
+    let bingoArr = "BINGO".split('').map(a => a.charCodeAt(0)-64)
+    for(let el of [...new Set(a)]){
+      if(bingoArr.includes(el)) sum++
+    }
+    return sum === 5 ? "WIN" : "LOSE"
+  }
