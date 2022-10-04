dbaf1ddf5d4a89a2892d383244c21f003b47359f
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-10-04 07:17:30 +0100

    numbers to letters

---
+++ 7kyu/numbers-to-letters.js
@@ -0,0 +1,22 @@
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+//                  solution
+
+const alpha = ' zyxwvutsrqponmlkjihgfedcba!? '
+const switcher = x => x.map(item => alpha[item]).join('')
+
+//                      solution
+
+function switcher(x)
+{
+  return x.map(a => 
+    {
+      if(a == 27) return "!";
+      if(a == 28) return "?";
+      if(a == 29) return " ";
+      return String.fromCharCode(97 + 26 - parseInt(a));
+      
+    }).join('');
+}
