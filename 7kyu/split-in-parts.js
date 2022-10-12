8a8523bc482a5e2f9a00ab517c69c7ac10cad9cb
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-10-12 15:44:32 +0100

    split in parts

---
+++ 7kyu/split-in-parts.js
@@ -0,0 +1,26 @@
+// The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+
+// Example:
+
+// Split the below string into other strings of size #3
+
+// 'supercalifragilisticexpialidocious'
+
+// Will return a new string
+// 'sup erc ali fra gil ist ice xpi ali doc iou s'
+// Assumptions:
+
+// String length is always greater than 0
+// String has no spaces
+// Size is always positive
+
+//                      solution
+
+var splitInParts = function(s, partLength){
+    var parts = [];
+    var array = s.split('');
+    while(array.length) {
+      parts.push(array.splice(0,partLength).join(''));
+    }
+    return parts.join(' ');
+  }
