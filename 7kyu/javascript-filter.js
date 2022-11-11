36796a21b19a776cf38c9c4d636baaf159ca7b9e
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-11-11 21:28:18 +0100

    javascript filter

---
+++ 7kyu/javascript-filter.js
@@ -0,0 +1,11 @@
+// While developing a website, you detect that some of the members have troubles logging in. Searching through the code you find that all logins ending with a "_" make problems. So you want to write a function that takes an array of pairs of login-names and e-mails, and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
+
+// If you have the input-array:
+
+// [ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+
+//                      solution
+
+function searchNames( logins ){
+    return logins.filter(([name,...rest])=>name.endsWith('_'));
+  }
