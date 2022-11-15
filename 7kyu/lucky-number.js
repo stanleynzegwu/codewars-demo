e7c0eecf306e4b80cec06a456a2668e96658ee79
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-11-15 09:37:29 +0100

    lucky number

---
+++ 7kyu/lucky-number.js
@@ -0,0 +1,18 @@
+// ###Lucky number
+
+// Write a function to find if a number is lucky or not. If the sum of all digits is 0 or multiple of 9 then the number is lucky.
+
+// 1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisible by 9, hence number is lucky.
+
+// Function will return true for lucky numbers and false for others.
+
+//                          solution
+
+function isLucky(n) {
+    let sum = String(n).split('').reduce((acc,c) => acc + +c,0)
+    return sum === 0 || sum % 9 === 0
+  }
+
+//                          solution
+
+const isLucky = n => n % 9 === 0;
