9bb1ca696e8af15a3d036a89056c29582a251b68
Stanley Nzegwu <nzegwustanley@gmail.com>
2022-12-23 22:13:35 +0100

    who is the killer

---
+++ 7kyu/who-is-the-killer.js
@@ -0,0 +1,21 @@
+// Some people have been killed!
+// You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+
+// Task.
+// Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+
+// {'James': ['Jacob', 'Bill', 'Lucas'],
+//  'Johnny': ['David', 'Kyle', 'Lucas'],
+//  'Peter': ['Lucy', 'Kyle']}
+// and also a list of the names of the dead people:
+
+// ['Lucas', 'Bill']
+// return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'
+
+//                          solution
+
+function killer(suspectInfo, dead) {
+    for(let key in suspectInfo){
+      if(suspectInfo[key].includes(...dead)) return key 
+    }
+  }
