4747859288756d1184e375dc11dcbd34f9fe0692
Stanley Nzegwu <nzegwustanley@gmail.com>
2023-02-27 06:29:27 +0100

    exclamation marks series 7

---
+++ 7kyu/exclamation-marks-series-7.js
@@ -0,0 +1,23 @@
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+
+// Examples
+// remove("Hi!") === ""
+// remove("Hi! Hi!") === ""
+// remove("Hi! Hi! Hi!") === ""
+// remove("Hi Hi! Hi!") === "Hi"
+// remove("Hi! !Hi Hi!") === ""
+// remove("Hi! Hi!! Hi!") === "Hi!!"
+// remove("Hi! !Hi! Hi!") === "!Hi!"
+
+//                      solution
+function remove(s){
+    return s.split(' ').filter(i => i.split('!').length != 2).join(' ');
+  }
+
+//                     solution
+function remove (string) {
+    let fil = string.split(' ').filter(word => {
+      return !word.includes('!') || (word.indexOf('!') !== word.lastIndexOf('!'))
+    })
+    return fil.join(' ')
+  }
