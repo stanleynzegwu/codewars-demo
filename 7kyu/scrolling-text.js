424bab13c42afeb7e55ad7ca8063ba267a8c4a86
Stanley Nzegwu <nzegwustanley@gmail.com>
2023-03-08 17:21:49 +0100

    scrolling text

---
+++ 7kyu/scrolling-text.js
@@ -0,0 +1,25 @@
+// Let's create some scrolling text!
+
+// Your task is to complete the function which takes a string, and returns an array with all possible rotations of the given string, in uppercase.
+
+// Example
+// scrollingText("codewars") should return:
+
+// [ "CODEWARS",
+//   "ODEWARSC",
+//   "DEWARSCO",
+//   "EWARSCOD",
+//   "WARSCODE",
+//   "ARSCODEW"
+//   "RSCODEWA",
+//   "SCODEWAR" ]
+
+//                      solution
+
+function scrollingText(text){
+    let arr = []
+    for(let i = 0; i < text.length; i++){
+      arr.push((text.slice(i) + text.slice(0,i)).toUpperCase())
+    }
+    return arr
+  }
