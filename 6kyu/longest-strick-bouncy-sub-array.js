4aa6a52d21d77342b1efbc2a385c9d694a463f56
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-04-02 19:07:58 +0100

    longest-strict-bouncy-subarray

---
+++ 6kyu/longest-strick-bouncy-sub-array.js
@@ -0,0 +1,47 @@
+// DESCRIPTION:
+// This kata was seen in programming competitions with a wide range of variations. A strict bouncy array of numbers, of length three or longer, is an array that each term (neither the first nor the last element) is strictly higher or lower than its neighbours.
+
+// For example, the array:
+
+// arr = [7,9,6,10,5,11,10,12,13,4,2,5,1,6,4,8] (length = 16)
+// The two longest bouncy subarrays of arr are
+
+// [7,9,6,10,5,11,10,12] (length = 8) and [4,2,5,1,6,4,8] (length = 7)
+// According to the given definition, the arrays [8,1,4,6,7], [1,2,2,1,4,5], are not bouncy.
+
+// For the special case of length 2 arrays, we will consider them strict bouncy if the two elements are different.
+
+// The arrays [-1,4] and [0,-10] are both bouncy, while [0,0] is not.
+
+// An array of length 1 will be considered strict bouncy itself.
+
+// You will be given an array of integers and you should output the longest strict bouncy subarray.
+
+// In the case of having more than one bouncy subarray of same length, it should be output the subarray with its first term of lowest index in the original array.
+
+// Let's see the result for the first given example.
+
+// arr = [7,9,6,10,5,11,10,12,13,4,2,5,1,6,4,8]
+// longest_bouncy_list(arr) === [7,9,6,10,5,11,10,12] 
+// See more examples in the example tests box.
+
+// Features of the random tests
+
+// length of the array inputs up to 1000
+// -500 <= arr[i] <= 500
+// Enjoy it!
+
+//                      solution
+
+function longestBouncyList(arr) {
+    var l=[arr[0]], m=l;
+    for(let i=0; i<arr.length; i++) {
+      if(l[l.length-1]!==arr[i]&&(l.length===1||(l[l.length-1]<l[l.length-2])!==(arr[i]<l[l.length-1]))) l.push(arr[i]);
+      else {
+        if(m.length<l.length) m=l;
+        if(i&&arr[i]!==arr[i-1]) i--;
+        l=[arr[i]];
+      }
+    }
+    return m.length<l.length?l:m;
+  }
