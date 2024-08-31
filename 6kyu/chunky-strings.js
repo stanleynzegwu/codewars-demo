d795e0c4edd4451889e716fa93721d71634a53cf
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-08-31 19:24:09 +0100

    chunky-strings

---
+++ 6kyu/chunky-strings.js
@@ -0,0 +1,48 @@
+// This kata concerns a string composed of 'chunks'.
+
+// A chunk is either:
+
+// A single alphabetic character
+// or:
+
+// Two chunks enclosed in square brackets [ and ]
+// A 'chunky string' is a string consisting of exactly one chunk. Here are some examples of chunky strings:
+
+// 'x'
+// '[xy]'
+// '[x[yy]]'
+// '[[xy]y]'
+// '[[ab][ab]]'
+// '[c[o[d[e[w[a[rs]]]]]]]'
+// For this task you will be given a valid chunky string from which all the closing brackets have been removed. You must restore the brackets and return the original valid chunky string.
+
+// For example:
+
+// Input               Restored String
+// 'x'                 'x'
+// '[xy'               '[xy]'
+// '[x[yy'             '[x[yy]]'
+// '[[xyy'             '[[xy]y]'
+// '[[ab[ab'           '[[ab][ab]]'
+// '[c[o[d[e[w[a[rs'   '[c[o[d[e[w[a[rs]]]]]]]'
+// In the tests all input strings will be valid chunky strings (with closing brackets removed).
+
+//                            solution
+
+function restoreBrackets(s) {
+    let res = [], stack = [], cur = 0;
+    for (const x of s) {
+      res.push(x);
+      if (x === '[') {
+        stack.push(cur);
+        cur = 0;
+      } else {
+        while (cur === 1) {
+          res.push(']');
+          cur = stack.pop();
+        }
+        cur++;
+      }
+    }
+    return res.join('');
+  }
